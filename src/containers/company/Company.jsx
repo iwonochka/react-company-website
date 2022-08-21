b360b1fbfd015e9fe69d31f313b769c610e097cf
Iwona Behnke <behnke.iwona@gmail.com>
2022-08-21 10:37:27 +0200

    Company component with css

--- src/containers/company/Company.jsx
+++ src/containers/company/Company.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './company.css'
 
 const Company = () => {
   return (
-    <div>Company</div>
+    <div className='company section__margin' id='company'>
+      <div className='company-feature'>
+        <Feature/>
+      </div>
+      <div className='company-header'>
+        <h1 className='gradient__text'>Technology that brings us forward</h1>
+        <p>Read more</p>
+      </div>
+      <div className='company-container'>
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
+
+    </div>
   )
 }
 
